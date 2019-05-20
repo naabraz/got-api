d4c31fbd56891f854a313b57e379d15d3c73a849
Natalia Braz <naabraz@gmail.com>
2019-05-20 18:53:06 -0300

    Changing get houses return

--- src/houses/houses.service.ts
+++ src/houses/houses.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class HousesService {
-  getHouses(): Array<string> {
-    return ['Targaryen', 'Stark', 'Lannister']
+  getHouses(): Array<Object> {
+    return [
+      { id: 1, name: 'Targaryen'}, 
+      { id: 2, name: 'Stark' }, 
+      { id: 3, name: 'Lannister'}
+    ]
   }
 }
